c06f7e142767730cc0821e2c35ffe653316caac7
Oisov <Oistein.Sovik.92@gmail.com>
2017-10-17 02:29:43 +0200

    Linted Project Euler 1

--- Problems/PE_001/Julia/PE_001.jl
+++ Problems/PE_001/Julia/PE_001.jl
@@ -1,4 +1,5 @@
-    using Combinatorics
+using Combinatorics
+
 
 function remove_non_coprime(divisors)
     new_divisors = []
@@ -12,31 +13,28 @@ function remove_non_coprime(divisors)
             if mod(divisor, divisors[index]) == 0
                 is_divisible = true
                 break
-            else
-                index += 1
             end
+            index += 1
         end
 
         if !is_divisible
             push!(new_divisors, divisor)
         end
     end
-    return new_divisors
+    new_divisors
 end
 
 
 function sum_divisible_by_k(k, start, stop)
     sum_start = div(start, k)
     sum_stop = div(stop - 1, k)
-    return div(k*(sum_stop+sum_start)*(sum_stop-sum_start+1),2)
+    div(k*(sum_stop+sum_start)*(sum_stop-sum_start+1),2)
 end
 
 
 function PE_001(div = [3, 5], stop = 10^3, start=1)
-
     divisors = remove_non_coprime(div)
     total = 0
-
     sign = 1
     for n in 1:length(divisors)
         for permutation in combinations(divisors, n)
@@ -45,7 +43,5 @@ function PE_001(div = [3, 5], stop = 10^3, start=1)
         end
         sign *= -1
     end
-    return total
+    total
 end
-
-println(PE_001([2,3,5,7], 10^6))
