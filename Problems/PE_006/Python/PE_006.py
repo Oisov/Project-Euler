32c379469887a64cbee57a9107ba53ce2c35b83d
Oisov <Oistein.Sovik.92@gmail.com>
2017-10-17 16:02:01 +0200

    Added solution to Project Euler 6

---
+++ Problems/PE_006/Python/PE_006.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+def PE_006(n=100):
+    '''
+    The sum of the first natural numbers are
+
+        S_1(n) = 1 + 2 + 3 ... n = n*(n+1)/2
+
+    The sum of the first square numbers are
+
+        S_2(n) = 1^2 + 2^3 + ... n^2 = n*(n+1)*(2*n+1)/6
+
+    By computing the difference of these two numbers one gets
+
+        S[1](n)^2 - S[2](n) = n (n - 1) (3n + 2) (n + 1) / 12
+    '''
+    return n * (n - 1) * (3 * n + 2) * (n + 1) / 12
+
+
+if __name__ == "__main__":
+
+    print PE_006()
