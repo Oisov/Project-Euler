91eb3e5ebb8b24d445e6c5ec51dd4c9f5aafef3f
Oisov <Oistein.Sovik.92@gmail.com>
2017-10-19 00:37:06 +0200

    Added solution to Project Euler 13 in Julia

---
+++ Problems/PE_013/Julia/PE_013.jl
@@ -0,0 +1,14 @@
+function first_n_digits(num, digits = 10)
+    BigInt(parse(Float64, string(BigInt(num))[1:digits]))
+end
+
+function PE_013(path="../PE_013_input.txt")
+    total = 0
+    f = open(path)
+    for line in readlines(f)
+        total += parse(Float64, line)
+    end
+    first_n_digits(total)
+end
+
+println(PE_013())
