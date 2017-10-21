4df88abe5f3a11a8860c4fe975492919cc18c231
Oisov <Oistein.Sovik.92@gmail.com>
2017-10-21 23:04:28 +0200

    Added solution to Project Euler 4 in Julia

---
+++ Problems/PE_004/Julia/PE_004.jl
@@ -0,0 +1,36 @@
+using Primes
+
+
+function number_of_digits(number)
+    Int(floor(log(10, number)+1))
+end
+
+
+function number_2_palindrome(number)
+    str_num = string(number)
+    parse(Int, str_num * reverse(str_num))
+end
+
+
+function PE_004(palindrome_length = 3, reverse_range = true)
+    max_num , min_num = 10^(palindrome_length)-1, 10^(palindrome_length-1)
+    range = reverse_range?(max_num:-1:min_num):(min_num:max_num)
+    for number in range
+
+        palindrome = number_2_palindrome(number)
+        smaller_palindrome = div(palindrome, 11)
+        if isprime(smaller_palindrome)
+            continue
+        end
+
+        smallest_factor = div(11*smaller_palindrome, 10^(palindrome_length))
+        for factor_1 = (10^(palindrome_length)-1):-1:(smallest_factor)
+            if smaller_palindrome % factor_1 == 0
+                factor_2 = div(smaller_palindrome, factor_1)
+                if number_of_digits(11*factor_2) == palindrome_length
+                    return (palindrome, " = ", factor_1, " * ", factor_2*11)
+                end
+            end
+        end
+    end
+end
