f760979ea4d6d6ce39ffb5846d37391f9e22cbc5
Oisov <Oistein.Sovik.92@gmail.com>
2017-10-26 01:47:51 +0200

    Apparently .el files updated, not sure what these emacs lips file do

--- Raport/auto/ProjectEuler.el
+++ Raport/auto/ProjectEuler.el
@@ -5,8 +5,22 @@
                      '(("article" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fontenc" "T1") ("inputenc" "utf8") ("babel" "english") ("xcolor" "dvpipsnames*" "svgnames") ("mdframed" "framemethod=TikZ") ("cleveref" "nameinlink") ("natbib" "numbers")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
+    "frontpage"
+    "preface"
+    "Problems/PE-002"
+    "Problems/PE-014"
     "article"
     "art10"
     "fontenc"
@@ -27,6 +41,7 @@
     "tikz"
     "mdframed"
     "listings"
+    "chngcntr"
     "geometry"
     "hyperref"
     "cleveref"
@@ -38,9 +53,12 @@
    (LaTeX-add-labels
     "PE-#2")
    (LaTeX-add-environments
-    '("ProjectEuler" LaTeX-env-args ["argument"] 1)
+    '("ProjectEuler" LaTeX-env-args ["argument"] 1))
+   (LaTeX-add-amsthm-newtheorems
     "lemma"
     "proposition"
-    "definition"))
+    "definition")
+   (LaTeX-add-caption-DeclareCaptions
+    '("\\DeclareCaptionFormat{listing}" "Format" "listing")))
  :latex)
 
--- Raport/auto/ProjectEuler2.el
+++ Raport/auto/ProjectEuler2.el
@@ -6,17 +6,18 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fontenc" "T1") ("inputenc" "utf8") ("babel" "english")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
+    "./Problems/PE_001"
     "article"
     "art10"
     "fontenc"
@@ -27,8 +28,6 @@
     "caption"
     "listings"
     "geometry")
-   (LaTeX-add-environments
-    '("ProjectEuler" LaTeX-env-args ["argument"] 1))
    (LaTeX-add-caption-DeclareCaptions
     '("\\DeclareCaptionFormat{listing}" "Format" "listing")))
  :latex)
