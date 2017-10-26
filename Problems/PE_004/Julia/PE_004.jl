cc4e1d6c1a8a36f1000415bb6393098eaf1f0bcc
Oisov <Oistein.Sovik.92@gmail.com>
2017-10-26 17:13:39 +0200

    PE 4; Added cheat for even numbers, restructured code

--- Problems/PE_004/Julia/PE_004.jl
+++ Problems/PE_004/Julia/PE_004.jl
@@ -1,32 +1,58 @@
 using Primes
 
 
-function number_of_digits(number)
-    Int(floor(log(10, number)+1))
-end
-
-
 function number_2_palindrome(number)
     str_num = string(number)
     parse(Int, str_num * reverse(str_num))
 end
 
 
+function is_palindrome(number)
+    number_str = string(number)
+    return number_str == reverse(number_str)
+end
+
+
+function temp_even_factors(length)
+    factor_1 = parse(BigInt, ("9"^length) * ("0"^(length-1)) * "1")
+    factor_2 = parse(BigInt, "9"^(2*length))
+    return (factor_1, factor_2)
+end
+
+
+function equal_number_of_digits(number_1, number_2)
+    return floor(log(10, number_1)) == floor(log(10, number_2))
+end
+
+
 function PE_004(palindrome_length = 3)
+
+    if palindrome_length % 2 == 0
+        temp_factor_1, temp_factor_2 = temp_even_factors(palindrome_length)
+        temp_palindrome = temp_factor_1 * temp_factor_2
+        if is_palindrome(temp_palindrome)
+            return temp_palindrome
+        end
+    end
+
     max_num , min_num = 10^(palindrome_length)-1, 10^(palindrome_length-1)
     for number = max_num:-1:min_num
 
         palindrome = number_2_palindrome(number)
         smaller_palindrome = div(palindrome, 11)
+
         if isprime(smaller_palindrome)
             continue
         end
 
-        smallest_factor = div(11*smaller_palindrome, 10^(palindrome_length))
-        for factor_1 = max_num:-1:(smallest_factor)
-            factor_2, remainder = divrem(smaller_palindrome, factor_1)
-            if remainder == 0 && number_of_digits(11*factor_2) == palindrome_length
-                return palindrome
+        smallest_possible_factor = div(palindrome, 10^(palindrome_length))
+        for number = max_num:-1:(smallest_possible_factor)
+            if smaller_palindrome % number == 0
+                factor_1 = number
+                factor_2 = 11*div(smaller_palindrome, factor_1)
+                if equal_number_of_digits(factor_1, factor_2)
+                    return (palindrome, factor_1, factor_2)
+                end
             end
         end
     end
