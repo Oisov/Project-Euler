fd520534861e7b355fcb74e0945e42fb22e1f4f4
Oisov <Oistein.Sovik.92@gmail.com>
2017-10-26 12:37:19 +0200

    PE 4; Removed wrong test, unncecerary reverse and cleaned code

--- Problems/PE_004/Julia/PE_004.jl
+++ Problems/PE_004/Julia/PE_004.jl
@@ -12,13 +12,9 @@ function number_2_palindrome(number)
 end
 
 
-function PE_004(palindrome_length = 3, reverse_range = true)
-    if palindrome_length == 1
-        return 9
-    end
+function PE_004(palindrome_length = 3)
     max_num , min_num = 10^(palindrome_length)-1, 10^(palindrome_length-1)
-    range = reverse_range?(max_num:-1:min_num):(min_num:max_num)
-    for number in range
+    for number = max_num:-1:min_num
 
         palindrome = number_2_palindrome(number)
         smaller_palindrome = div(palindrome, 11)
@@ -28,11 +24,9 @@ function PE_004(palindrome_length = 3, reverse_range = true)
 
         smallest_factor = div(11*smaller_palindrome, 10^(palindrome_length))
         for factor_1 = (10^(palindrome_length)-1):-1:(smallest_factor)
-            if smaller_palindrome % factor_1 == 0
-                factor_2 = div(smaller_palindrome, factor_1)
-                if number_of_digits(11*factor_2) == palindrome_length
-                    return palindrome
-                end
+            factor_2, remainder = divrem(smaller_palindrome, factor_1)
+            if remainder == 0 && number_of_digits(11*factor_2) == palindrome_length
+                return palindrome
             end
         end
     end
